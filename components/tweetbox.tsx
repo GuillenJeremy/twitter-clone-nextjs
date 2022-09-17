1b8da2f1383efe3d1e5dd932cc306cbd128079b4
Jeremy Guillen <guillenjeremy26@gmail.com>
2022-09-16 18:53:58 -0600

    adding tweetbox

---
+++ components/tweetbox.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Tweetbox() {
+    return (
+        <div>
+            <img src="https://links.papareact.com/gll" alt=""/>
+            
+        </div>
+    )
+}
