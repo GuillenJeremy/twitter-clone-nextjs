62fe93a18ef1fa67d2e2e65b2ebd8625b1783cfc
Jeremy Guillen <guillenjeremy26@gmail.com>
2022-11-29 19:36:11 -0600

    installed sanity

--- components/tweetbox.tsx
+++ components/tweetbox.tsx
@@ -11,7 +11,7 @@ export function Tweetbox() {
 
     return (
         <div className='flex space-x-2 p- 5'>
-            <img className='h-14 2-14 rounded-full object-cover mt-4' src="https://links.papareact.com/gll" alt=""/>
+            <img className='h-14 rounded-full object-cover mt-4' src="https://links.papareact.com/gll" alt=""/>
             <div className='flex flex-1 items-center pl-2'>
                 <form className='flex flex-1 flex-col'>
                     <input value={input} type='text' placeholder="What's happening?" className='h-24 w-full text-xl outline-none placeholder:text-xl' onChange={(e) => setInput(e.target.value)}/>
@@ -19,10 +19,10 @@ export function Tweetbox() {
                         <div className='flex space-x-2 text-twitter flex-1'>
                             {/* ICONS */}
                             <CameraIcon className='h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150'/>
-                            <MagnifyingGlassIcon className='h-5 w-5 cursor-pointer'/>
-                            <FaceSmileIcon className='h-5 w-5 cursor-pointer' />
-                            <CalendarIcon className='h-5 w-5 cursor-pointer' />
-                            <MapPinIcon className='h-5 w-5 cursor-pointer' />
+                            <MagnifyingGlassIcon className='h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150'/>
+                            <FaceSmileIcon className='h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150' />
+                            <CalendarIcon className='h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150' />
+                            <MapPinIcon className='h-5 w-5 cursor-pointer transition-transform duration-150 ease-out hover:scale-150' />
                         </div>
                         <button disabled={!input} className='bg-twitter px-5 py-2 font-bold text-white rounded-full'>Tweet</button>
                     </div>
