80aeccf2e7606859f330f98bca13928e60c99f34
jeremyguillen26 <jeremy.guillen@linkam.com>
2022-12-04 14:26:13 -0600

    adding comments section

--- components/tweet.tsx
+++ components/tweet.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import TimeAgo from 'react-time-ago';
 import { Tweet } from '../typings';
 import {HeartIcon, ChatBubbleBottomCenterIcon, ArrowsRightLeftIcon, ArrowUpOnSquareIcon} from '@heroicons/react/24/outline'
-
+import { fetchComments } from '../utils/fetchComments';
+import {Comment} from '../typings';
 
 interface Props {
     tweet: Tweet;
 };
 
 function Tweet({tweet}: Props) {
+    const [comments, setComments] = useState<Comment[]>([]);
+    const refreshComments = async () => {
+        const comments: Comment[] = await fetchComments(tweet._id);
+        console.log(comments);
+        setComments(comments);
+    }
+    useEffect(() => {
+        refreshComments();
+    }, []);
     return (
         <div className='flex flex-col space-x-3 border-y border-gray-100 p-5'>
             <div className='flex space-x-3'>
@@ -39,6 +49,25 @@ function Tweet({tweet}: Props) {
                     <ArrowsRightLeftIcon className='h-5 w-5' />
                 </div>
             </div>
+            {/* Comments */}
+
+            {comments?.length > 0 && (
+                <div className='my-2 mt-5 max-h-44 space-y-5 overflow-y-scroll border-t border-gray-100 p-5'>
+                    {comments.map(comment => (
+                        <div key={comment._id} className='flex space-x-2'>
+                            <img src={comment.profileImg} alt="user img" className='h-7 w-7 object-cover rounded-full'/>
+                            <div>
+                                <div className='flex items-center space-x-1'>
+                                    <p className='mr-1 font-bold'>{comment.username}</p>
+                                    <p className='hidden text-sm text-gray-500 lg:inline'>@{comment.username.replace(/\s+/g, '').toLowerCase()}</p>
+                                    <TimeAgo className='text-sm text-gray-500' date={new Date(comment._createdAt)} locale="en-US"/>
+                                </div>
+                                <p>{comment.comment}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
--- typings.d.ts
+++ typings.d.ts
@@ -1,3 +1,10 @@
+export interface TweetBody {
+  text: string
+  username: string
+  profileImg: string
+  image?: string
+}
+
 export interface Tweet extends Tweetbody {
   _id: string
   _createdAt: string
@@ -11,9 +18,23 @@ export interface Tweet extends Tweetbody {
   image?: string
 }
 
-export interface TweetBody {
-  text: string
-  username: string
-  profileImg: string
-  image?: string
+
+
+export interface CommentBody {
+  comment: string;
+  tweetId: string;
+  username: string;
+  profileImg: string;
+}
+
+export interface Comment extends CommentBody {
+  _id: string
+  _createdAt: string
+  _updatedAt: string
+  _rev: string
+  _type: 'comment',
+  tweet: {
+    _ref: string
+    _type: 'reference'
+  }
 }
